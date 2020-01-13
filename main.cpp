a72a648ff4f8f0dab475594799c26e047182f703
Ushitora Anqou <ushitora@anqou.net>
2020-01-13 23:15:06 +0900

    Add testPlainFromJSONtest_and_4bit

--- main.cpp
+++ main.cpp
@@ -160,10 +160,35 @@ void testPlainFromJSONtest_pass_4bit()
     assert(net.output("io_out", 3).task->get() == 0);
 }
 
+void testPlainFromJSONtest_and_4bit()
+{
+    const std::string fileName = "test/test-and-4bit.json";
+    std::ifstream ifs{fileName};
+    assert(ifs);
+
+    auto net = readNetworkFromJSON<PlainNetworkBuilder>(ifs);
+    net.input("io_inA", 0).task->set(0);
+    net.input("io_inA", 1).task->set(0);
+    net.input("io_inA", 2).task->set(1);
+    net.input("io_inA", 3).task->set(1);
+    net.input("io_inB", 0).task->set(0);
+    net.input("io_inB", 1).task->set(1);
+    net.input("io_inB", 2).task->set(0);
+    net.input("io_inB", 3).task->set(1);
+
+    processAllGates(net, 3);
+
+    assert(net.output("io_out", 0).task->get() == 0);
+    assert(net.output("io_out", 1).task->get() == 0);
+    assert(net.output("io_out", 2).task->get() == 0);
+    assert(net.output("io_out", 3).task->get() == 1);
+}
+
 int main()
 {
     testPlainBinopGates();
     testPlainMUX();
     testPlainNOT();
     testPlainFromJSONtest_pass_4bit();
+    testPlainFromJSONtest_and_4bit();
 }
