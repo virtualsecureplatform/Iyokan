3afc09363446df61147fbe1f8fb5aab212953895
Ushitora Anqou <ushitora@anqou.net>
2020-03-15 12:39:20 +0900

    [test.rb] Add test_in_out and improve readability

--- test.rb
+++ test.rb
@@ -14,6 +14,15 @@ def quote(str, prefix = "> ")
   prefix + str.gsub("\n", "\n#{prefix}")
 end
 
+def toml2packet(toml)
+  {
+    cycles: toml["cycles"],
+    ram: toml["ram"].map { |v| [v["name"], v] }.to_h,
+    rom: toml["rom"].map { |v| [v["name"], v] }.to_h,
+    bits: toml["bits"].map { |v| [v["name"], v] }.to_h,
+  }
+end
+
 ##### assert #####
 
 def assert_equal(got, expected)
@@ -28,31 +37,30 @@ def assert_regex(str, reg)
   raise "Assert failed: assert_regex #{reg.inspect}\n#{quote(str)}" unless str =~ reg
 end
 
-def check_code_detail(command, args)
+def assert_equal_packet(got, expected)
+  assert_equal toml2packet(got), toml2packet(expected)
+end
+
+def run_command(command, args = [])
   path = Pathname.new($path) / command
   res, _, code = Open3.capture3("#{Shellwords.join([path.to_s] + args)} 2>&1")
   raise "Unexpected status code: #{code}\n#{quote(res)}" if code != 0
   res
 end
 
-def print_error(fh, command, args, msg)
+def run_iyokan(args)
+  run_command "./iyokan", args
+end
+
+def run_iyokan_packet(args)
+  run_command "./iyokan-packet", args
+end
+
+def print_error(fh, command, args, ex)
   $has_any_error = true
   fh.puts "\e[31m[ERROR] #{command.inspect} [#{args.map { |s| s.inspect }.join(", ")}]\e[m"
-  fh.puts msg
+  fh.puts ex.full_message
   fh.puts
-end
-
-def check_code(command, args = [])
-  check_code_detail(command, args)
-rescue => ex
-  print_error $stderr, "check_code", [command] + args, ex.message
-end
-
-def test_iyokan(args)
-  res = check_code_detail "./iyokan", args
-  yield res
-rescue => ex
-  print_error $stderr, "test_iyokan", args, ex.message
 end
 
 raise "ruby test.rb PATH [slow]" unless ARGV.size >= 1
@@ -65,226 +73,94 @@ $CUDA_MODE_ENABLED = ARGV.include?("cuda")
 
 ##### test0 #####
 
-check_code "./test0"
+run_command "./test0"
 
 ##### iyokan #####
 
-check_code "./iyokan-packet", ["toml2packet",
-                               "--in", "test/test00-packet.toml",
-                               "--out", "_test_plain_req_packet00"]
-check_code "./iyokan-packet", ["toml2packet",
-                               "--in", "test/test01-packet.toml",
-                               "--out", "_test_plain_req_packet01"]
+def test_in_out(blueprint, in_file, out_file, args0 = [], args1 = [], args2 = [])
+  plain_req_file = "_test_plain_req_packet"
+  plain_res_file = "_test_plain_res_packet"
+  req_file = "_test_req_packet"
+  res_file = "_test_res_packet"
+  secret_key = "_test_sk"
+  cycles = -1
 
-test_iyokan [
-  "plain",
-  "--blueprint", "test/cahp-diamond.toml",
-  "-i", "_test_plain_req_packet00",
-  "-o", "_test_plain_res_packet00",
-] do |_|
-  r = check_code "./iyokan-packet", ["unpack", "--in", "_test_plain_res_packet00"]
-  toml = TomlRB.parse(r)
-  assert_equal toml["cycles"].to_i, 8
-  assert_include toml["bits"], { "bytes" => [1], "size" => 1, "name" => "finflag" }
-  assert_include toml["bits"], { "bytes" => [42, 0], "size" => 16, "name" => "reg_x0" }
+  run_iyokan_packet ["toml2packet",
+                     "--in", in_file,
+                     "--out", plain_req_file]
+
+  ## Check plain mode
+  run_iyokan ["plain",
+              "--blueprint", blueprint,
+              "-i", plain_req_file,
+              "-o", plain_res_file] + args0
+  r = run_iyokan_packet ["unpack", "--in", plain_res_file]
+  got = TomlRB.parse(r)
+  expected = TomlRB.load_file(out_file)
+  assert_equal_packet got, expected
+  yield 0 if block_given?
+  cycles = got["cycles"]  # Get # of cycles for the rest
+
+  if $SLOW_MODE_ENABLED
+    run_iyokan_packet ["enc",
+                       "--key", secret_key,
+                       "--in", plain_req_file,
+                       "--out", req_file]
+
+    ## Check TFHE mode
+    run_iyokan ["tfhe",
+                "--blueprint", blueprint,
+                "-c", cycles,
+                "-i", req_file,
+                "-o", res_file] + args1
+    run_iyokan_packet ["dec",
+                       "--key", secret_key,
+                       "--in", res_file,
+                       "--out", plain_res_file]
+    r = run_iyokan_packet ["unpack", "--in", plain_res_file]
+    got = TomlRB.parse(r)
+    expected = TomlRB.load_file(out_file)
+    assert_equal_packet got, expected
+    yield 1 if block_given?
+
+    if $CUDA_MODE_ENABLED
+      ## Check cuFHE mode
+      run_iyokan ["tfhe",
+                  "--enable-gpu",
+                  "--blueprint", blueprint,
+                  "-c", cycles,
+                  "-i", req_file,
+                  "-o", res_file] + args2
+      run_iyokan_packet ["dec",
+                         "--key", secret_key,
+                         "--in", res_file,
+                         "--out", plain_res_file]
+      r = run_iyokan_packet ["unpack", "--in", plain_res_file]
+      got = TomlRB.parse(r)
+      expected = TomlRB.load_file(out_file)
+      assert_equal_packet got, expected
+      yield 2 if block_given?
+    end
+  end
+rescue => ex
+  print_error $stderr, "test_in_out", [blueprint, in_file, out_file], ex
 end
 
-test_iyokan [
-  "plain",
-  "--blueprint", "test/cahp-diamond.toml",
-  "-i", "_test_plain_req_packet01",
-  "-o", "_test_plain_res_packet01",
-] do |_|
-  r = check_code "./iyokan-packet", ["unpack", "--in", "_test_plain_res_packet01"]
-  toml = TomlRB.parse(r)
-  assert_equal toml["cycles"].to_i, 346
-  assert_include toml["bits"], { "bytes" => [1], "size" => 1, "name" => "finflag" }
-  assert_include toml["bits"], { "bytes" => [5, 0], "size" => 16, "name" => "reg_x8" }
+run_iyokan_packet ["genkey", "--type", "tfhepp", "--out", "_test_sk"]
+test_in_out "test/cahp-diamond.toml", "test/test00.in", "test/test00-diamond.out"
+test_in_out "test/cahp-emerald.toml", "test/test00.in", "test/test00-emerald.out"
+test_in_out "test/cahp-diamond.toml", "test/test01.in", "test/test01-diamond.out"
+test_in_out "test/cahp-emerald.toml", "test/test01.in", "test/test01-emerald.out"
 
-  assert_include toml["ram"], { "bytes" => [0] * 256, "size" => 2048, "name" => "ramA" }
-  assert_include toml["ram"], {
-                   "bytes" => [0] * 239 +
-                              [1, 2, 39, 1, 2, 39, 1, 3, 39, 3, 5, 39, 0, 0, 59, 5, 0],
-                   "size" => 2048,
-                   "name" => "ramB",
-                 }
-end
-
-test_iyokan [
-  "plain",
-  "--blueprint", "test/cahp-emerald.toml",
-  "-i", "_test_plain_req_packet01",
-  "-o", "_test_plain_res_packet01",
-] do |_|
-  r = check_code "./iyokan-packet", ["unpack", "--in", "_test_plain_res_packet01"]
-  toml = TomlRB.parse(r)
-  assert_equal toml["cycles"].to_i, 261
-  assert_include toml["bits"], { "bytes" => [1], "size" => 1, "name" => "finflag" }
-  assert_include toml["bits"], { "bytes" => [5, 0], "size" => 16, "name" => "reg_x8" }
-
-  assert_include toml["ram"], { "bytes" => [0] * 256, "size" => 2048, "name" => "ramA" }
-  assert_include toml["ram"], {
-                   "bytes" => [0] * 239 +
-                              [1, 2, 39, 1, 2, 39, 1, 3, 39, 3, 5, 39, 0, 0, 59, 5, 0],
-                   "size" => 2048,
-                   "name" => "ramB",
-                 }
-end
-
-test_iyokan [
-  "plain",
-  "--blueprint", "test/cahp-diamond.toml",
-  "-i", "_test_plain_req_packet00",
-  "-o", "_test_plain_res_packet00",
-  "--dump-prefix", "_test_dump",
-] do |r|
-  r = check_code "./iyokan-packet", ["unpack", "--in", "_test_dump-7"]
+test_in_out "test/cahp-diamond.toml", "test/test00.in", "test/test00-diamond.out",
+            ["--dump-prefix", "_test_dump"],
+            ["--dump-prefix", "_test_dump", "--secret-key", "_test_sk"],
+            ["--dump-prefix", "_test_dump", "--secret-key", "_test_sk"] do |_|
+  r = run_iyokan_packet ["unpack", "--in", "_test_dump-7"]
   toml = TomlRB.parse(r)
   assert_equal toml["cycles"].to_i, 7
   assert_include toml["bits"], { "bytes" => [0], "size" => 1, "name" => "finflag" }
   assert_include toml["bits"], { "bytes" => [42, 0], "size" => 16, "name" => "reg_x0" }
-end
-
-if $SLOW_MODE_ENABLED
-  check_code "./iyokan-packet", ["genkey", "--type", "tfhepp", "--out", "_test_sk"]
-  check_code "./iyokan-packet", ["enc",
-                                 "--key", "_test_sk",
-                                 "--in", "_test_plain_req_packet00",
-                                 "--out", "_test_req_packet00"]
-  check_code "./iyokan-packet", ["enc",
-                                 "--key", "_test_sk",
-                                 "--in", "_test_plain_req_packet01",
-                                 "--out", "_test_req_packet01"]
-
-  test_iyokan [
-    "tfhe",
-    "--blueprint", "test/cahp-diamond.toml",
-    "-i", "_test_req_packet00",
-    "-o", "_test_res_packet00",
-    "-c", "8",
-  ] do |_|
-    check_code "./iyokan-packet", ["dec",
-                                   "--key", "_test_sk",
-                                   "--in", "_test_res_packet00",
-                                   "--out", "_test_plain_res_packet00"]
-    r = check_code "./iyokan-packet", ["unpack", "--in", "_test_plain_res_packet00"]
-    toml = TomlRB.parse(r)
-    assert_equal toml["cycles"].to_i, 8
-    assert_include toml["bits"], { "bytes" => [1], "size" => 1, "name" => "finflag" }
-    assert_include toml["bits"], { "bytes" => [42, 0], "size" => 16, "name" => "reg_x0" }
-  end
-
-  test_iyokan [
-    "tfhe",
-    "--blueprint", "test/cahp-diamond.toml",
-    "-i", "_test_req_packet00",
-    "-o", "_test_res_packet00",
-    "-c", "8",
-    "--dump-prefix", "_test_dump",
-    "--secret-key", "_test_sk",
-  ] do |r|
-    r = check_code "./iyokan-packet", ["unpack", "--in", "_test_dump-7"]
-    toml = TomlRB.parse(r)
-    assert_equal toml["cycles"].to_i, 7
-    assert_include toml["bits"], { "bytes" => [0], "size" => 1, "name" => "finflag" }
-    assert_include toml["bits"], { "bytes" => [42, 0], "size" => 16, "name" => "reg_x0" }
-  end
-
-  if $CUDA_MODE_ENABLED
-    test_iyokan [
-      "tfhe",
-      "--blueprint", "test/cahp-diamond.toml",
-      "-i", "_test_req_packet00",
-      "-o", "_test_res_packet00",
-      "-c", "8",
-      "--enable-gpu",
-    ] do |_|
-      check_code "./iyokan-packet", ["dec",
-                                     "--key", "_test_sk",
-                                     "--in", "_test_res_packet00",
-                                     "--out", "_test_plain_res_packet00"]
-      r = check_code "./iyokan-packet", ["unpack", "--in", "_test_plain_res_packet00"]
-      toml = TomlRB.parse(r)
-      assert_equal toml["cycles"].to_i, 8
-      assert_include toml["bits"], { "bytes" => [1], "size" => 1, "name" => "finflag" }
-      assert_include toml["bits"], { "bytes" => [42, 0], "size" => 16, "name" => "reg_x0" }
-    end
-
-    test_iyokan [
-      "tfhe",
-      "--blueprint", "test/cahp-diamond.toml",
-      "-i", "_test_req_packet00",
-      "-o", "_test_res_packet00",
-      "-c", "8",
-      "--enable-gpu",
-      "--dump-prefix", "_test_dump",
-      "--secret-key", "_test_sk",
-    ] do |r|
-      check_code "./iyokan-packet", ["dec",
-                                     "--key", "_test_sk",
-                                     "--in", "_test_res_packet00",
-                                     "--out", "_test_plain_res_packet00"]
-      r = check_code "./iyokan-packet", ["unpack", "--in", "_test_dump-7"]
-      toml = TomlRB.parse(r)
-      assert_equal toml["cycles"].to_i, 7
-      assert_include toml["bits"], { "bytes" => [0], "size" => 1, "name" => "finflag" }
-      assert_include toml["bits"], { "bytes" => [42, 0], "size" => 16, "name" => "reg_x0" }
-    end
-
-    test_iyokan [
-      "tfhe",
-      "--blueprint", "test/cahp-diamond.toml",
-      "-i", "_test_req_packet01",
-      "-o", "_test_res_packet01",
-      "-c", "346",
-      "--enable-gpu",
-    ] do |_|
-      check_code "./iyokan-packet", ["dec",
-                                     "--key", "_test_sk",
-                                     "--in", "_test_res_packet01",
-                                     "--out", "_test_plain_res_packet01"]
-      r = check_code "./iyokan-packet", ["unpack", "--in", "_test_plain_res_packet01"]
-      toml = TomlRB.parse(r)
-      assert_equal toml["cycles"].to_i, 346
-      assert_include toml["bits"], { "bytes" => [1], "size" => 1, "name" => "finflag" }
-      assert_include toml["bits"], { "bytes" => [5, 0], "size" => 16, "name" => "reg_x8" }
-
-      assert_include toml["ram"], { "bytes" => [0] * 256, "size" => 2048, "name" => "ramA" }
-      assert_include toml["ram"], {
-                       "bytes" => [0] * 239 +
-                                  [1, 2, 39, 1, 2, 39, 1, 3, 39, 3, 5, 39, 0, 0, 59, 5, 0],
-                       "size" => 2048,
-                       "name" => "ramB",
-                     }
-    end
-
-    test_iyokan [
-      "tfhe",
-      "--blueprint", "test/cahp-emerald.toml",
-      "-i", "_test_req_packet01",
-      "-o", "_test_res_packet01",
-      "-c", "261",
-      "--enable-gpu",
-    ] do |_|
-      check_code "./iyokan-packet", ["dec",
-                                     "--key", "_test_sk",
-                                     "--in", "_test_res_packet01",
-                                     "--out", "_test_plain_res_packet01"]
-      r = check_code "./iyokan-packet", ["unpack", "--in", "_test_plain_res_packet01"]
-      toml = TomlRB.parse(r)
-      assert_equal toml["cycles"].to_i, 261
-      assert_include toml["bits"], { "bytes" => [1], "size" => 1, "name" => "finflag" }
-      assert_include toml["bits"], { "bytes" => [5, 0], "size" => 16, "name" => "reg_x8" }
-
-      assert_include toml["ram"], { "bytes" => [0] * 256, "size" => 2048, "name" => "ramA" }
-      assert_include toml["ram"], {
-                       "bytes" => [0] * 239 +
-                                  [1, 2, 39, 1, 2, 39, 1, 3, 39, 3, 5, 39, 0, 0, 59, 5, 0],
-                       "size" => 2048,
-                       "name" => "ramB",
-                     }
-    end
-  end
 end
 
 exit 1 if $has_any_error
