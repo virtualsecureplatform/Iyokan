3f771097fd25151b719a94b290c3c5316b91333c
Ushitora Anqou <ushitora@anqou.net>
2021-04-01 17:54:36 +0900

    [test.rb] Add test rom-7-32

--- test.rb
+++ test.rb
@@ -364,6 +364,8 @@ reg.add_in_out "ram-8-16-16-08", "test/test-ram-8-16-16.toml",
                "test/test08.in", "test/test08.out", ncycles: 8, set_plain_ncycles: true
 reg.add_in_out "mux-ram-8-16-16-08", "test/test-mux-ram-8-16-16.toml",
                "test/test08.in", "test/test08.out", ncycles: 8, set_plain_ncycles: true
+reg.add_in_out "rom-7-32", "test/test-rom-7-32.toml",
+               "test/test12.in", "test/test12.out", ncycles: 1, set_plain_ncycles: true
 
 reg.add_in_out("cahp-diamond-dump-prefix-00", "test/cahp-diamond.toml",
                "test/test00.in", "test/test00-diamond.out",
