d3138bf19c7c74da75f70a5066ae13b8e8bf0bf3
Ushitora Anqou <ushitora@anqou.net>
2021-04-20 09:51:37 +0900

    Revert "fixup"
    This reverts commit caddf91926fa6533a9940efb9d5c8eccaee4eae0.

--- iyokan_cufhe.hpp
+++ iyokan_cufhe.hpp
@@ -7,7 +7,7 @@
 #include "iyokan.hpp"
 #include "iyokan_tfhepp.hpp"
 
-inline void cufheNot(cufhe::Ctxt& out, const cufhe::Ctxt& in, cufhe::Stream st)
+void cufheNot(cufhe::Ctxt& out, const cufhe::Ctxt& in, cufhe::Stream st)
 {
     for (int i = 0; i <= in.lwe_sample_->n(); i++)
         out.lwe_sample_->data()[i] = -in.lwe_sample_->data()[i];
