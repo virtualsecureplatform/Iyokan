0c97fa602f45f36d934c9138973c9eb11745677e
Ushitora Anqou <ushitora@anqou.net>
2021-09-09 20:49:48 +0900

    [test/master-chisel/build.sh] Minimize resulted JSON files

--- test/master-chisel/build.sh
+++ test/master-chisel/build.sh
@@ -34,16 +34,24 @@ abc -g gates,MUX
 clean -purge
 
 # write synthesized design
-write_json $module.json
+write_json ${module}.json
 EOS
     yosys _build.ys
+    # Thanks to: https://stackoverflow.com/a/7729087
+    cat "${module}.json" | \
+        jq -c "del(..|.port_directions?) | `
+              `del(..|.parameters?) | `
+              `del(..|.attributes?) | `
+              `del(..|.hide_name?)" > "${module}.min.json"
+    rm "${module}.json"
 done
 
-mv Addr4bit.json addr-4bit-yosys.json
-mv And4_2bit.json addr-4_2bit-yosys.json
-mv And4bit.json and-4bit-yosys.json
-mv Counter4bit.json counter-4bit-yosys.json
-mv Divider8bit.json div-8bit-yosys.json
-mv Mux4bit.json mux-4bit-yosys.json
-mv Pass4bit.json pass-4bit-yosys.json
-mv Register4bit.json register-4bit-yosys.json
+mv Addr4bit.min.json addr-4bit-yosys.json
+mv And4_2bit.min.json addr-4_2bit-yosys.json
+mv And4bit.min.json and-4bit-yosys.json
+mv BigMult.min.json big-mult-yosys.json
+mv Counter4bit.min.json counter-4bit-yosys.json
+mv Divider8bit.min.json div-8bit-yosys.json
+mv Mux4bit.min.json mux-4bit-yosys.json
+mv Pass4bit.min.json pass-4bit-yosys.json
+mv Register4bit.min.json register-4bit-yosys.json
